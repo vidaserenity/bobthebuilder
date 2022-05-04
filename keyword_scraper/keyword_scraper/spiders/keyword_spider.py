36c575c3bc46cb07d6c2e3ace0d6ed367016d53e
EGZJ17 <55767984+EGZJ17@users.noreply.github.com>
2022-05-04 15:34:22 -0700

    Update

--- keyword_scraper/keyword_scraper/spiders/keyword_spider.py
+++ keyword_scraper/keyword_scraper/spiders/keyword_spider.py
@@ -1,18 +1,19 @@
 import scrapy
 
-class keyword_spider(scrapy.Spider):
-    name = "keyword_scraper"
 
-    start_urls = ["https://www.theladders.com/career-advice/1000-industry-specific-keywords-that-will-make-your-resume-unstoppable-according-to-experts"]
+class keyword_spider(scrapy.Spider):
+    name = 'keyword_scraper'
+
+    start_urls = ['https://www.theladders.com/career-advice/1000-industry-specific-keywords-that-will-make-your-resume-unstoppable-according-to-experts']
 
     def parse(self,response):
         career = []
-        article = response.css('article.article-content').get()
+        article = response.css("article.article-content").get()
         # for x in article.css():
         #     pass
 
-        for headings in article.css('h3'):
-            career += headings.css('::text').get()
+        for headings in article.css("h3"):
+            career += headings.css("::text").get()
             # grabs all the text under headings
             # appends them to list named career
             # we'd probably have to clean it afterwards
@@ -20,3 +21,16 @@ class keyword_spider(scrapy.Spider):
             # career = [headings.css('::text') for headings in article.css('h3')]
         
         yield career   
+
+'''
+class keyword_spider(scrapy.Spider):
+    name = "keyword_scraper"
+
+    start_urls = ["https://www.theladders.com/career-advice/1000-industry-specific-keywords-that-will-make-your-resume-unstoppable-according-to-experts"]
+
+    def parse(self,response):
+        
+        for career in response.css("article.article-content")
+            element = response.css("h3")
+        career = .get()
+'''
